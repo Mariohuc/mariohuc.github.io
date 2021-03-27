3cd69a14b26da0c04d68020c0da459d1cd13e1e1
Mariohuc <mhuaypuna@unsa.edu.pe>
2021-03-27 02:56:39 -0500

    Added deploy.sh and google html key for indexing

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+#-------------------------------------------
+# IMPORTANT! PASSWORD: mario
+#-------------------------------------------
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/mariohuc/mariohuc.github.io.git master
+
+cd -
